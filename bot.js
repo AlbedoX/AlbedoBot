b1b9233c0c3a6841cba3f2bdd4b0d04395fd7f96
AlbedoX <39501275+AlbedoX@users.noreply.github.com>
2024-06-23 12:37:19 +0200

    Changes to one of the "Jokes"

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-// Configure logger settings
+
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
@@ -28,17 +28,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Wucky!'
                 });
-                break; // This is to quit out of the switch statement
+                break; 
             case 'Mom´s':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Spaghetti!'
                 });
                 break;
-                case 'Tim':
+                case 'Caity':
                     bot.sendMessage({
                         to: channelID,
-                        message: 'is my Father!'
+                        message: 'is my Mother!'
                     });
                     break;
         }
